01dd25b5bcdf47d8544b3828a7d694abb69816f7
PavelVoitov <voitov.pavel@yandex.ru>
2023-02-09 00:39:45 +0300

    added changes for pagination added REDUX_DEVTOOLS

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,4 +1,4 @@
-import {combineReducers, legacy_createStore, Store, applyMiddleware} from "redux";
+import {combineReducers, legacy_createStore, applyMiddleware, compose} from "redux";
 import {ProfileActionsTypes, profileReducer} from "./profile-reducer";
 import {MessagesActionType, messagesReducer} from "./messages-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
@@ -8,9 +8,9 @@ import thunkMiddleware, {ThunkAction} from 'redux-thunk';
 import { reducer as formReducer } from 'redux-form'
 import {appReducer, InitializedAppActionType} from "./app-reducer";
 
-export type ReducerPropsType = ReturnType<typeof reducer>
+export type ReducerPropsType = ReturnType<typeof reducers>
 
-const reducer = combineReducers({
+const reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messagesReducer,
     sidebar: sidebarReducer,
@@ -20,7 +20,18 @@ const reducer = combineReducers({
     form: formReducer
 })
 
-export const store: Store<ReducerPropsType> = legacy_createStore(reducer, applyMiddleware(thunkMiddleware))
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = legacy_createStore(reducers, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
+
+// export const store: Store<ReducerPropsType> = legacy_createStore(reducer, applyMiddleware(thunkMiddleware))
 
 export type AppActionsType = MessagesActionType | ProfileActionsTypes | UsersActionType | InitializedAppActionType
 
